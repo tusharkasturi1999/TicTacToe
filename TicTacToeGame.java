f185f52f0b1509df9547b4ee265530ff7ba2ad85
tusharkasturi1999 <tusharkasturi1999@gmail.com>
2021-08-23 16:09:49 +0530

    Added PlayerChoice method for the player to select 'X' or 'O'

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -1,15 +1,18 @@
+
 /**
  * @author tushar.kasturi_ymedi
  *
  */
+import java.util.*;
 
 public class TicTacToeGame {
 
 	private static char[] board = new char[10];
+	private static char playerInput;
+	public static final Scanner sc = new Scanner(System.in);
 
 	/**
 	 * This method is used to create board and initialize it
-	 *
 	 *
 	 */
 
@@ -20,11 +23,15 @@ public class TicTacToeGame {
 		}
 	}
 
+	public static void playerChoice() {
+		System.out.println("Choose 'X' or 'O' ");
+		playerInput = sc.next().charAt(0);
+	}
+
 	public static void main(String[] args) {
 
-		
 		System.out.println("Welcome to Tic Tac Toe \n");
 		createBoard();
-
+		playerChoice();
 	}
 }
