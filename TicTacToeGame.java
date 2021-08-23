7146f2fb6b09ba7eb9d80745890be3ad3ab70191
tusharkasturi1999 <tusharkasturi1999@gmail.com>
2021-08-23 09:51:16 +0530

    Added Board Creation

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -1,4 +1,30 @@
+/**
+ * @author tushar.kasturi_ymedi
+ *
+ */
 
 public class TicTacToeGame {
 
+	private static char[] board = new char[10];
+
+	/**
+	 * This method is used to create board and initialize it
+	 *
+	 *
+	 */
+
+	public static void createBoard() {
+
+		for (int i = 1; i < board.length; i++) {
+			board[i] = ' ';
+		}
+	}
+
+	public static void main(String[] args) {
+
+		
+		System.out.println("Welcome to Tic Tac Toe \n");
+		createBoard();
+
+	}
 }
